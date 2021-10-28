89c0eb2fc7589208609a33b5d9545fe1cf414685
Roman Koifman <Romansko@users.noreply.github.com>
2021-10-28 20:27:16 +0300

    Power Hungry question initial upload

---
+++ level2/PowerHungry/solution.py
@@ -0,0 +1,12 @@
+# Python 2.7.13
+# level2 - Power Hungry
+# https://github.com/Romansko/Foobar/blob/main/level2/PowerHungry/solution.py
+
+def solution(xs):
+    pass
+
+
+if __name__ == '__main__':
+    # Some tests
+    pass
+
