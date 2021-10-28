4adebbd2779745495ea3e712c7c6a534a11e0e2b
Roman Koifman <Romansko@users.noreply.github.com>
2021-10-28 22:25:21 +0300

    Power Hungry Solution

--- level2/PowerHungry/solution.py
+++ level2/PowerHungry/solution.py
@@ -3,10 +3,52 @@
 # https://github.com/Romansko/Foobar/blob/main/level2/PowerHungry/solution.py
 
 def solution(xs):
-    pass
+    if not xs or type(xs) is not list:
+        return str(0)
+
+    # Build positive and negative numbers list
+    posList = filter(lambda x: x > 0, xs)
+    negList = filter(lambda x: x < 0, xs)
+
+    # This shit took me 1 hour to detect. if xs is a single negative element, return that element..
+    if (len(xs) == 1) and (xs[0] < 0):
+        return str(xs[0])
+
+    # remove first occurrence of unproductive odd number if negative list is odd.
+    if len(negList) % 2:
+        negList.remove(max(negList))
+
+    # empty lists.. (check after possible removal of negative item).
+    if (len(posList) == 0) and (len(negList) == 0):
+        return str(0)
+
+    # calculate product and return as string
+    product = 1
+    for n in posList:
+        product *= n
+    for n in negList:
+        product *= n
+    return str(product)  # required str on return
 
 
 if __name__ == '__main__':
     # Some tests
-    pass
-
+    tests = [
+        [[2, 0, 2, 2, 0], str(8)],
+        [[-2, -3, 4, -5], str(60)],
+        [[1000] * 50, str(pow(1000, 50))],  # maximum values
+        [[-1], str(-1)],
+        [[-1, 0], str(0)],
+        [[1], str(1)],
+        [[0], str(0)],
+        [[0, 0], str(0)],
+        [[0, 0, -15], str(0)],
+        [[-1, -1], str(1)],
+        [[-1, -1, -1], str(1)],
+        [[-1, -1, 1], str(1)],
+        [[-1, 1, 1], str(1)],
+        [[1, 1, 1], str(1)]
+    ]
+    for test, expected in tests:
+        assert expected == solution(test)
+    print("Tests done")
